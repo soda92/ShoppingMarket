fb7add33dccad7c94533c2fb40e38acef872326e
SodaCris <2231993478@qq.com>
2020-02-28 00:05:17 +0800

    how to test using selenium...

---
+++ src/test/java/Test1.java
@@ -0,0 +1,17 @@
+// import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.extension.ExtendWith;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import io.github.bonigarcia.seljup.SeleniumExtension;
+
+// @ExtendWith(SeleniumExtension.class)
+public class Test1 {
+
+    // @Test
+    // public void testLocalChrome(ChromeDriver driver) {
+    //     driver.get("http://localhost:8080/webdemo");
+    //     System.out.println("waiting");
+    //     // driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    //     driver.quit();
+    // }
+
+}
